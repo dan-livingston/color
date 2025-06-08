3cf9032dbbf4e1fbc4b2eb72b1459c28a4536894
Daniel Livingston <contact@daniellivingston.net>
2025-06-08 12:13:33 -0700

    remove unnecessary files

--- src/conversions/hexToHsl.ts
+++
@@ -1,8 +0,0 @@
-import type { Hsl } from '../types';
-import { hexToRgb } from './hexToRgb';
-import { rgbToHsl } from './rgbToHsl';
-
-export function hexToHsl(value: number): Hsl {
-	const { r, g, b } = hexToRgb(value);
-	return rgbToHsl(r, g, b);
-}
--- src/conversions/hslToHex.ts
+++
@@ -1,7 +0,0 @@
-import { hslToRgb } from './hslToRgb';
-import { rgbToHex } from './rgbToHex';
-
-export function hslToHex(h: number, s: number, l: number): number {
-	const { r, g, b } = hslToRgb(h, s, l);
-	return rgbToHex(r, g, b);
-}
