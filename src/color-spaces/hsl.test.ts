68c9479050d59700d537c4d4081fc197a4151976
Daniel Livingston <contact@daniellivingston.net>
2025-06-07 18:59:12 -0700

    add hsl color space

---
+++ src/color-spaces/hsl.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+import { hsl } from './hsl';
+import * as Color from '../colors';
+
+const colors = [Color.RED, Color.GREEN, Color.BLUE, Color.WHITE, Color.BLACK];
+
+describe('hsl', () => {
+	it('initializes a color from a valid hsl', () => {
+		for (const color of colors) {
+			expect(() => hsl(color.h, color.s, color.l)).not.toThrow();
+		}
+	});
+
+	it("doesn't initialize a color from an invalid hsl", () => {
+		expect(() => hsl(-1, 0, 0)).toThrow();
+		expect(() => hsl(361, 0, 0)).toThrow();
+		expect(() => hsl(0, -1, 0)).toThrow();
+		expect(() => hsl(0, 2, 0)).toThrow();
+		expect(() => hsl(0, 0, -1)).toThrow();
+		expect(() => hsl(0, 0, 2)).toThrow();
+	});
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { pipe } from './actions/pipe';
 export { hex } from './color-spaces/hex';
+export { hsl } from './color-spaces/hsl';
 export { rgb } from './color-spaces/rgb';
 export { set } from './transforms/set';
 export type { Color, HexString } from './types';
